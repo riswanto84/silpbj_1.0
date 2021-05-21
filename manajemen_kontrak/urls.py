f5394c41203bf72e296e2114762ffad3bd0a1d53
root <root@simple.lpse.kemsos.net>
2021-05-21 10:48:09 +0000

    pengaturan email

--- manajemen_kontrak/urls.py
+++ manajemen_kontrak/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
@@ -38,4 +39,12 @@ urlpatterns = [
     path('foto_item_pekerjaan/<str:pk>',
          views.foto_item_pekerjaan, name='foto_item_pekerjaan'),
     path('tanda_terima_distribusi/<str:pk>', views.tanda_terima_distribusi, name='tanda_terima_distribusi'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
