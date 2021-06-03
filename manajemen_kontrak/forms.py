49264249bad1c43f278ce7575c1eb209d212b505
Riswanto <riswanto.aris@gmail.com>
2021-06-03 11:14:37 +0700

    salah

--- manajemen_kontrak/forms.py
+++ manajemen_kontrak/forms.py
@@ -9,7 +9,7 @@ class UserAdminForm(ModelForm):
     class Meta:
         model = UserAdmin
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user', 'bagian']
 
         labels = {
             'nama': _('Nama Lengkap'),
