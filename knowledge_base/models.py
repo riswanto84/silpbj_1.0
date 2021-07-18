19d954df8a986fc1cb98a406a688c33b0ac75112
riswanto <riswanto.aris@gmail.com>
2021-07-18 08:29:52 +0000

    Minggu 18 Juli 2021

--- knowledge_base/models.py
+++ knowledge_base/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
+
+class FilePost(models.Model):
+  file_info = models.FileField(upload_to='file_knowledge_base')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -14,12 +18,14 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
+    gambar = models.ImageField(upload_to='gambar_knowledge_base', blank=True, null=True)
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
     author = models.ForeignKey(User,
                               on_delete=models.CASCADE,
                               related_name='blog_posts')
     body = models.TextField()
+    file = models.FileField(upload_to='file_knowledge_base', blank=True, null=True)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -29,6 +35,7 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manager
     published = PublishedManager() # Our custom manager
+    tags = TaggableManager()
     
     class Meta:
         ordering = ('-publish',)
